81d7dc056494df850b4ee1ef6b82ae05794a6de4
dojo24 <mlongenberger@codingdojo.com>
2021-02-19 09:27:54 -0500

    a few changes made css working now.  pushing and testing deploy

--- myBlog/settings.py
+++ myBlog/settings.py
@@ -16,7 +16,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIRS = os.path.join(BASE_DIR,'templates')
-STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Quick-start development settings - unsuitable for production
@@ -54,6 +55,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'myBlog.urls'
+
 
 TEMPLATES = [
     {
